0f2b4509865e10d45e879cc855f5e13fc0da665a
aranzazuvera2005-design <aranzazu.vera.2005@gmail.com>
2026-03-03 17:07:54 +0100

    Update import path for AgregarAlCarritoBtn

--- components/SearchProductos.tsx
+++ components/SearchProductos.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import AgregarAlCarritoBtn from '@/AgregarAlCarritoBtn';
+import AgregarAlCarritoBtn from '@/components/AgregarAlCarritoBtn';
 
 
 export default function SearchProductos({ initialProducts = [], initialQuery = '' }: { initialProducts?: any[], initialQuery?: string }) {

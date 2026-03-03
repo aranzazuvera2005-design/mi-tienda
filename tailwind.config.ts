f7be5789d6fede671892a27ba08638006fce9da4
aranzazuvera2005-design <aranzazu.vera.2005@gmail.com>
2026-03-03 17:32:41 +0100

    Update Tailwind config content paths
    Removed unnecessary line from content array in Tailwind config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-  "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./context/**/*.{js,ts,jsx,tsx,mdx}", // IMPORTANTE: Añade esta línea
-],
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./context/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {

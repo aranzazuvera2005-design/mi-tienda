d5558dcd365c422e106531cc4906822212410ee9
aranzazuvera2005-design <aranzazu.vera.2005@gmail.com>
2026-03-05 21:20:41 +0100

    Comment out Toaster import in layout.tsx
    Comment out the import statement for Toaster component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 import Header from "@/components/Header";
-import { Toaster } from "@/components/ui/toaster"; 
+// import { Toaster } from "@/components/ui/toaster"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
